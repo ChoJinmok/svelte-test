75bea83e853fc0806a52fa53afec5e821ec590fc
ChoJinmok <jjmok1127@gmail.com>
2023-01-29 18:54:12 +0900

    Add: 반복문

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,22 @@
 <script lang="ts">
-  const name = 'world';
-  let toggle = false;
+  const name = 'Fruits';
+  let fruits = ['Apple', 'Banana', 'Cherry', 'Orange', 'Mango'];
 
-  function handleToggleButtonClick() {
-    toggle = !toggle;
+  function deleteFruit() {
+    fruits = fruits.slice(1);
   }
-
-// svelte의 특수문자
-  // - `#`: 시작
-  // - `:`: 중간
-  // - `/`: 끝
-  // if문 외 each, await 구문도 모두 동일함
 </script>
 
-<!-- if 문 -->
-<section>
-  <button on:click={handleToggleButtonClick}>
-    Toggle
-  </button>
-  {#if toggle}
-    <h1>Hello {name}!</h1>
-  {/if}
-</section>
-
-<!-- if - else 문 -->
-<section>
-  <button on:click={handleToggleButtonClick}>
-    Toggle
-  </button>
-  {#if toggle}
-    <h1>Hello {name}!</h1>
-  {:else}
-    <h2>No name!</h2>
-  {/if}
-</section>
+<h1>Hello {name}!</h1>
+<ul>
+  <!-- Svelte의 반복문 -->
+  {#each fruits as fruit}
+    <li>{fruit}</li>
+  {/each}
+</ul>
+<button on:click={deleteFruit}>
+  Eat it!
+</button>
 
 <style
   global
