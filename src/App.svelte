9d9c0c5637eb257d262200a8ebf9223a2aa39a9d
ChoJinmok <jjmok1127@gmail.com>
2023-02-01 03:14:55 +0900

    Add: 시작하기 / 4. 컴포넌트

--- src/App.svelte
+++ src/App.svelte
@@ -1,68 +1,41 @@
 <script lang="ts">
-  // `onMount`는 컴포넌트가 연결(mount)되면 콜백을 실행한다.
-  // import { onMount } from 'svelte';
+  import Fruits from './Fruits.svelte';
 
-  let name = 'world';
-  let isRed = false;
-  let text = '';
-
-  function handleBoxClick() {
-    isRed = !isRed;
-  }
-
-  // 현재 컴포넌트가 준비되면 onMount로 전달한 콜백 함수가 동작한다.
-  // onMount(() => {
-  //   const box = document.querySelector('.box');
-
-  //   box.addEventListener('click', handleBoxClick);
-  // });
-
-  // 하지만 위와 같이 addEventListener 방식으로 코드를 작성하면 번거로워진다.
-  // -> Svelte에서 제공해주는 api를 사용하는 것이 더 간단하다.
-  // -> html태그에 on 디렉티브(키워드)를 작성해주면 된다.
-  // on 디렉티브에는 addEventListener에 붙일 수 있는 모든 이벤트들을 모두 작성할 수 있다.
-
-  function handleBoxMouseEnter() {
-    name = 'enter';
-  }
-
-  function handleBoxMouseLeave() {
-    name = 'leave';
-  }
-
-  function handleInput({ target }: Event) {
-    const { value } = target as HTMLInputElement;
-
-    text = value;
-  }
-
-  function handleButtonClick() {
-    text = 'Jinmok';
-  }
+  const fruits = ['Apple', 'Banana', 'Cherry', 'Orange', 'Mango'];
 </script>
 
-<h1>Hello {name}!</h1>
-<div
-  class="box w-80 h-40"
-  class:bg-orange-400={!isRed}
-  class:bg-red-600={isRed}
-  on:click={handleBoxClick}
-  on:keydown={handleBoxClick}
-  on:mouseenter={handleBoxMouseEnter}
-  on:mouseleave={handleBoxMouseLeave}
->Box!</div>
-<h1>{text}</h1>
-<input
-  type="text"
-  value={text}
-  on:input={handleInput}
-/>
-<input
-  type="text"
-  bind:value={text}
-/>
-<!-- 위의 input 모두 양방향 바인딩을 해준 것이지만 `bind`사용하면 더 간단하게 양방향바인딩을 해줄 수 있다. -->
-<button on:click={handleButtonClick}>Click</button>
+<Fruits {fruits} />
+<Fruits
+  {fruits}
+  reverse />
+<Fruits
+  {fruits}
+  slice="-2" />
+<Fruits
+  {fruits}
+  slice="0, 3" />
+
+<!-- 똑같은 구조가 반복되는 것을 볼 수 있다. -> 컴포넌트화 -->
+<!-- <h2>Fruits</h2>
+<ul>
+  {#each fruits as fruit}
+    <li>{fruit}</li>
+  {/each}
+</ul>
+
+<h2>Fruits reverse</h2>
+<ul>
+  {#each [...fruits].reverse() as fruit}
+    <li>{fruit}</li>
+  {/each}
+</ul>
+
+<h2>Fruits slice -2</h2>
+<ul>
+  {#each fruits.slice(-2) as fruit}
+    <li>{fruit}</li>
+  {/each}
+</ul> -->
 
 <style
   global
---
+++ src/Fruits.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  // Props
+  // eslint-disable-next-line import/no-mutable-exports
+  export let fruits: string[];
+  // eslint-disable-next-line import/no-mutable-exports
+  export let reverse = false;
+  // eslint-disable-next-line import/no-mutable-exports
+  export let slice = '';
+  // export라서 내보내는 것 같지만 내보내는 것이 아니라 부모 컴포넌트와 연결할 수 있는 통로를 만드는 작업이라고 보면 좋겠다.
+
+  function computeFruits() {
+    if (reverse) return [...fruits].reverse();
+
+    if (slice) {
+      const slicePrams = slice.split(',').map((pram) => Number(pram));
+
+      return [...fruits].slice(...slicePrams);
+    }
+
+    return fruits;
+  }
+
+  function makeTitle() {
+    if (reverse) return 'reverse';
+
+    if (slice) return `slice ${slice}`;
+
+    return '';
+  }
+
+  const title = `Fruits ${makeTitle()}`;
+  const computedFruits = computeFruits();
+</script>
+
+<h2>{title}</h2>
+<ul>
+  {#each computedFruits as fruit}
+    <li>{fruit}</li>
+  {/each}
+</ul>
+
+<style
+  global
+  lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
