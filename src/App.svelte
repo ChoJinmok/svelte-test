8f7ec36a996f817786a320ca4fbeaa037dc99769
ChoJinmok <jjmok1127@gmail.com>
2023-02-05 00:17:20 +0900

    Add: 라이프사이클 / 1. 기본 개념

--- src/App.svelte
+++ src/App.svelte
@@ -1,53 +1,34 @@
 <script lang="ts">
-  import { writable } from 'svelte/store';
+  import Something from './Something.svelte';
 
-  import Todo from './Todo.svelte';
-
-  let title = '';
-  const todos = writable<{ id: number, title: string }[]>([]);
-  let id = 0;
-
-  function createTodo() {
-    if (!title.trim()) {
-      title = '';
-
-      return;
-    }
-
-    const newTodos = [...$todos];
-
-    newTodos.push({
-      id,
-      title,
-    });
-
-    // 반응성을 유지시켜야하기 때문에 할당을 다시 해줘야한다.
-    // 자기가 자기 자신에게 재할당하는 것도 재할당으로 인식한다. -> `todos = todos;`로 작성가능함
-    $todos = newTodos;
-
-    title = '';
-    id += 1;
-  }
-
-  function handleTodoTitleKeyDown({ key }: KeyboardEvent) {
-    if (key === 'Enter') {
-      createTodo();
-    }
-
-  // 아래와 같이 요약 가능 -> 단, eslint no-unused-expressions 에러 발생
-    // key === 'Enter' && createTodo();
-  }
+  let toggle = false;
 </script>
 
-<input
-  type="text"
-  bind:value={title}
-  on:keydown={handleTodoTitleKeyDown}
-/>
-<button on:click={createTodo}>Create Todo</button>
+<button on:click={() => { toggle = !toggle; }}>
+  Toggle
+</button>
 
-{#each $todos as todo}
-  <Todo
-    {todo}
-    {todos} />
-{/each}
+// Lifecycle은 컴포넌트가 연결되고 해제되는 등의 컴포넌트의 생명주기로 원하는 코드를 실행할 수 있도록 여러 기능을 제공한다.
+
+// Toggle이 true가 되고 Something이 렌더링 될 때 "연결이 되었다(Mounted)"고 포현한다. -> `onMount()`
+// Toggle이 false가 되고 Something이 사라졌을 때 "연결이 해제되었다(Destroyed)"고 포현한다. -> `onDestroy()`
+{#if toggle}
+  <Something />
+{/if}
+
+// toggle에 의해 반응성을 가짐
+// -> 반응성을 가지는 데이터를 다룰 때
+// "화면이 바뀌기 전(Before update)" -> `beforeUpdate()` -> 화면이 바뀌기 전 상태를 제어할 수 있다.
+// "화면이 바뀐 후(After update)" -> `afterUpdate()` -> 화면이 바뀌고 난 상태를 제어할 수 있다.
+
+// [컴포넌트를 연결하면, (최초)] -> beforeUpdate -|-> onMount -> afterUpdate | onDestroy(연결시엔 실행 X)
+//                               DOM과 데이터를 동기화(렌더링)
+
+// [컴포넌트를 연결 해제하면] -> onDestroy (다른 Lifecycle 생략)
+// 완벽하게 떨어지기 전 = 분리가 되기 전에 = 연결이 실제 해제되기 직전에 onDestroy가 실행된다.
+
+// [컴포넌트의 데이터를 갱신하면] -> beforeUpdate -|-> afterUpdate (onMount 생략)
+//                               DOM과 데이터를 동기화(렌더링)
+
+// 각 Lifecycle을 Lifecycle Hook(훅, 후크)라고도 부른다.
+// E.g. onMount Hook, beforeUpdate Hook
