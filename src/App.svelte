41438135737ba60f742cd92bc896a582e69aa3fe
ChoJinmok <jjmok1127@gmail.com>
2023-02-06 02:15:59 +0900

    Add: 라이프사이클 / 3. onMount, onDestroy

--- src/App.svelte
+++ src/App.svelte
@@ -2,38 +2,16 @@
   import Something from './Something.svelte';
 
   let toggle = false;
+
+  function handleToggleButtonClick() {
+    toggle = !toggle;
+  }
 </script>
 
-<button on:click={() => { toggle = !toggle; }}>
+<button on:click={handleToggleButtonClick}>
   Toggle
 </button>
-
-<!--
-Lifecycle은 컴포넌트가 연결되고 해제되는 등의 컴포넌트의 생명주기로 원하는 코드를 실행할 수 있도록 여러 기능을 제공한다.
-
-Toggle이 true가 되고 Something이 렌더링 될 때 "연결이 되었다(Mounted)"고 포현한다. -> `onMount()`
-Toggle이 false가 되고 Something이 사라졌을 때 "연결이 해제되었다(Destroyed)"고 포현한다. -> `onDestroy()`
--->
 
 {#if toggle}
   <Something />
 {/if}
-
-<!--
-toggle에 의해 반응성을 가짐
--> 반응성을 가지는 데이터를 다룰 때
-"화면이 바뀌기 전(Before update)" -> `beforeUpdate()` -> 화면이 바뀌기 전 상태를 제어할 수 있다.
-"화면이 바뀐 후(After update)" -> `afterUpdate()` -> 화면이 바뀌고 난 상태를 제어할 수 있다.
-
-[컴포넌트를 연결하면, (최초)] -> beforeUpdate -|-> onMount -> afterUpdate | onDestroy(연결시엔 실행 X)
-                             DOM과 데이터를 동기화(렌더링)
-
-[컴포넌트를 연결 해제하면] -> onDestroy (다른 Lifecycle 생략)
-완벽하게 떨어지기 전 = 분리가 되기 전에 = 연결이 실제 해제되기 직전에 onDestroy가 실행된다.
-
-[컴포넌트의 데이터를 갱신하면] -> beforeUpdate -|-> afterUpdate (onMount 생략)
-                             DOM과 데이터를 동기화(렌더링)
-
-각 Lifecycle을 Lifecycle Hook(훅, 후크)라고도 부른다.
-E.g. onMount Hook, beforeUpdate Hook
--->
--- src/Something.svelte
+++ src/Something.svelte
@@ -1,3 +1,35 @@
-<h1>
-  Something
-</h1>
+<script lang="ts">
+  import { onMount, onDestroy } from 'svelte';
+
+  //   아래의 코드는 아직 컴포넌트가 화면에 렌더링 되기 전에 동작하기 때문에 h1태그를 못 찾는다.
+//   const h1 = document.querySelector('h1');
+//   console.log(h1.innerText);
+
+  onMount(() => {
+    console.log('Mounted!');
+
+    // const h1 = document.querySelector('h1');
+    // console.log(h1.innerText);
+
+    // onMount에서 반환하는 함수는 컴포넌트가 연결 해제될 때 실행된다.
+    // onDestroy와 동일하게 동작
+    // 원래 onDestroy는 없었고 onMount에서 반환하는 함수로 로직을 처리해줬었다.
+    // 그런데 여러가지 이슈때문에 onDestroy가 생겼다.
+    // 실행 순서: onDestroy -> onMount에서 반환하는 함수
+    // 둘중 하나를 선택하면 된다.(두가지 모두 활용할 필요 없다.)
+    // 만약 onMount의 콜백함수가 async 함수인 경우 promise객체를 반환하게 되면서 아래 코드가 정상적으로 동작하지 않는다. -> 동기 함수일 때만 사용가능
+    return () => {
+      console.log('Destory in mount');
+    };
+  });
+
+  onDestroy(() => {
+    console.log('Destoryed!');
+
+  // onDestroy의 callback 함수는 컴포넌트가 연결 해제되기 직전에 코드가 실행되기 때문에 아래코드가 정상적으로 작동한다.
+    // const h1 = document.querySelector('h1');
+    // console.log(h1.innerText);
+  });
+</script>
+
+<h1>Something..</h1>
