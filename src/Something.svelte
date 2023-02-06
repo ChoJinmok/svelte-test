1249e34426897920deb5e71197101e1756809456
ChoJinmok <jjmok1127@gmail.com>
2023-02-07 01:45:15 +0900

    Add: 라이프사이클 / 4. beforeUpdate, afterUpdate

--- src/Something.svelte
+++ src/Something.svelte
@@ -1,12 +1,34 @@
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte';
+  import {
+    onMount, onDestroy, beforeUpdate, afterUpdate,
+  } from 'svelte';
+
+  let name = 'Something..';
+  let h1;
+
+  function moreDot() {
+    name += '.';
+  }
 
   //   아래의 코드는 아직 컴포넌트가 화면에 렌더링 되기 전에 동작하기 때문에 h1태그를 못 찾는다.
 //   const h1 = document.querySelector('h1');
 //   console.log(h1.innerText);
 
+// beforeUpdate(화면이 바뀓기전)와 afterUpdate(화면이 바뀐 후)는 반응성을 가지는 데이터가 할당을 통해서 갱신이 되면 실행된다.
+// 컴포넌트가 연결될 때도 동작한다.
+// 반응성을 가지는 로직이 beforeUpdate와 afterUpdate안에 있으면 무한 루프에 빠진다. -> 꼭 넣어야하는 상황이면 조건문으로 데이터가 갱신되지 않도록 한다.
+  beforeUpdate(() => {
+    console.log('Before update!');
+
+    // 컴포넌트가 화면에 연결되서 화면에 출력되긴 전엔 h1은 undefined
+    console.log(h1 && h1.innerText);
+  // 마운트된 후 h1을 클릭하면 .이 추가 되기 전에 출력된다.
+  });
+
   onMount(() => {
     console.log('Mounted!');
+
+    h1 = document.querySelector('h1');
 
     // const h1 = document.querySelector('h1');
     // console.log(h1.innerText);
@@ -18,9 +40,16 @@
     // 실행 순서: onDestroy -> onMount에서 반환하는 함수
     // 둘중 하나를 선택하면 된다.(두가지 모두 활용할 필요 없다.)
     // 만약 onMount의 콜백함수가 async 함수인 경우 promise객체를 반환하게 되면서 아래 코드가 정상적으로 동작하지 않는다. -> 동기 함수일 때만 사용가능
-    return () => {
-      console.log('Destory in mount');
-    };
+    // return () => {
+    //   console.log('Destory in mount');
+    // };
+  });
+
+  afterUpdate(() => {
+    console.log('After update!');
+
+    console.log(h1.innerText);
+  // 점이 추가된 h1이 출력
   });
 
   onDestroy(() => {
@@ -30,6 +59,7 @@
     // const h1 = document.querySelector('h1');
     // console.log(h1.innerText);
   });
+
 </script>
 
-<h1>Something..</h1>
+<h1 on:click={moreDot}>{name}</h1>
