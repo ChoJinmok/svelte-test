bbd53a37561e65ac58588f3697dfbd996a50410a
ChoJinmok <jjmok1127@gmail.com>
2023-02-07 22:53:27 +0900

    Add: 라이프사이클 / 6. tick

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
-  import { lifecycle, delayRender } from './lifecycle';
+  // tick: 데이터가 갱신되고 나서 화면이 바뀌는 반응성을 가질 때까지 잠시 기다릴 수 있게 해줌
+  import { tick } from 'svelte';
 
-  import Something from './Something.svelte';
+  let name = 'world';
 
-  const done = delayRender();
+  async function handleTitleClick() {
+    name = 'Jinmok';
 
-  lifecycle();
+    // tick함수는 프로미스 객체를 반환한다. -> 비동기로 실행해야한다.
+    await tick();
+
+    const h1 = document.querySelector('h1');
+    // 갱신된 Jinmok이 콘솔창에 입력될 거 같지만 world가 입력된다.
+    // 스벨트는 전체 로직의 최적화를 위해 기본적인 로직들을 모두 실행하고 나면 화면을 갱신하도록 설계되었다.
+    // 화면이 갱신된 이후에 처리해야할 로직이 있다면 tick을 사용하여 화면이 갱신될 때까지 기다린다.
+    console.log(h1.innerText);
+  }
+
+// onMount, onDestroy, beforeUpdate, afterUpdate는 언제 실행되는지가 정해져 있지만 tick은 실행 위치를 내가 정할 수 있다.
 </script>
 
-{#if $done}
-  <h1>Hello Lifecycle!</h1>
-{/if}
-
-<Something />
+<h1
+  on:click={handleTitleClick}
+  on:keydown={handleTitleClick}>Hello {name}!</h1>
--- src/Something.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { delayRender } from './lifecycle';
-
-  const done = delayRender({ delay: 1000 });
-</script>
-
-{#if $done}
-  <h1>Something...</h1>
-{/if}
