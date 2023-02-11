c19d9913441155db94d5808081a406ad2cf1099f
ChoJinmok <jjmok1127@gmail.com>
2023-02-11 20:27:22 +0900

    Add: 반응성 / 5. 반응성 구문 패턴 정

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,77 @@
 <script lang="ts">
-  import { tick } from 'svelte';
-
   let count = 0;
-  let double = 0;
 
-  // 스벨트에서 Label의 별칭이 `$`인 경우에만 반응성 구문으로 동작
-  // 스벨트의 반응성 구문 내부에 반응성을 가지는 데이터(count)가 명시돼있으면
-  // 반응성을 가지는 데이터가 갱신이 되면 반응성 구문이 동작한다.
-  // 반응성 구문은 반응성 데이터 값이 변경된 후 대기 로직이 처리되고 하면이 갱신되고 나서 동작한다.
-  $: {
-    double = count * 2;
-    console.log('double!');
+  const { log } = console;
+
+  function fnLog() {
+    log('fn: Jinmok!');
   }
 
-  // 미리 작성돼있지 않은 데이터를 작성해줘도 동작한다.
-  // $: double = count * 2;
-
-  async function assign() {
+  function assign() {
     count += 1;
-    console.time('timer');
-    // double이 한박자 늦게 동작한다.
-    await tick();
-    console.timeEnd('timer');
-    console.log(double);
+  }
+
+  // 선언
+  // var로 변수가 생성되는 것이 아니라 Svelte가 let을 붙여준다.
+  $: double = count * 2;
+
+  // 블록
+  $: {
+    log(count);
+    log(double);
+  }
+
+  // 함수 실행
+  $: count, fnLog();
+
+  // 즉시 실행 함수(IIFE)
+  $: count, (() => {
+    log('iife: Jinmok');
+  })();
+
+  // 조건문(If)
+  // 해당하는 반응성 구문이 최초 렌더링시 실행되지 않길 바란다면 조건문 활용
+  $: if (count > 0) {
+    log('if:', double);
+  }
+
+  // 반복문(For)
+  $: for (let i = 0; i < 3; i += 1) {
+    count;
+    log('for:', i);
+  }
+
+  // 조건문(Switch)
+  $: switch (count) {
+    case 1:
+      log('switch: 1');
+      break;
+    default:
+      log('switch: default');
+  }
+
+  // 유효범위
+  // 반응성 구문 내부에 어떤 방식으로든 반응성 데이터가 들어가 있으면 데이터가 갱신돌 때 코드가 실행된다.
+  // 코드블록의 depth와는 관계 없이 동작
+  $: {
+    function scope1() {
+      log('scope1');
+
+      function scope2() {
+        log('scope2');
+
+        function scope3() {
+          log('scope3', count);
+        }
+
+        scope3();
+      }
+
+      scope2();
+    }
+
+    scope1();
   }
 </script>
 
 <button on:click={assign}>Assign!</button>
-
-<h2>{count}</h2>
-<h2>{double}</h2>
