b71a093536e4f0af387b249ecd5fa8c704a0809b
ChoJinmok <jjmok1127@gmail.com>
2023-02-12 00:10:22 +0900

    Add: 클래스와 스타일 / 1. 클래스와 스타일 속성 바인딩

--- src/App.svelte
+++ src/App.svelte
@@ -1,77 +1,56 @@
 <script lang="ts">
-  let count = 0;
+  let active = false;
+  // const color = 'tomato';
+  // const white = 'white';
+  const color = {
+    t: 'tomato',
+    w: '#fff',
+  };
+  const letterSpacing = 'letter-spacing: 5px;';
 
-  const { log } = console;
-
-  function fnLog() {
-    log('fn: Jinmok!');
-  }
-
-  function assign() {
-    count += 1;
-  }
-
-  // 선언
-  // var로 변수가 생성되는 것이 아니라 Svelte가 let을 붙여준다.
-  $: double = count * 2;
-
-  // 블록
-  $: {
-    log(count);
-    log(double);
-  }
-
-  // 함수 실행
-  $: count, fnLog();
-
-  // 즉시 실행 함수(IIFE)
-  $: count, (() => {
-    log('iife: Jinmok');
-  })();
-
-  // 조건문(If)
-  // 해당하는 반응성 구문이 최초 렌더링시 실행되지 않길 바란다면 조건문 활용
-  $: if (count > 0) {
-    log('if:', double);
-  }
-
-  // 반복문(For)
-  $: for (let i = 0; i < 3; i += 1) {
-    count;
-    log('for:', i);
-  }
-
-  // 조건문(Switch)
-  $: switch (count) {
-    case 1:
-      log('switch: 1');
-      break;
-    default:
-      log('switch: default');
-  }
-
-  // 유효범위
-  // 반응성 구문 내부에 어떤 방식으로든 반응성 데이터가 들어가 있으면 데이터가 갱신돌 때 코드가 실행된다.
-  // 코드블록의 depth와는 관계 없이 동작
-  $: {
-    function scope1() {
-      log('scope1');
-
-      function scope2() {
-        log('scope2');
-
-        function scope3() {
-          log('scope3', count);
-        }
-
-        scope3();
-      }
-
-      scope2();
-    }
-
-    scope1();
+  function handleToggleButtonClick() {
+    active = !active;
   }
 </script>
 
-<button on:click={assign}>Assign!</button>
+<button on:click={handleToggleButtonClick}
+>
+  Toggle!
+</button>
+
+<!-- Svelte에서 `:`앞에 붙는 단어를 지시어(directive)라고 부른다. (e.g. class directive) -->
+<!-- <div class={active ? 'active' : ''}> -->
+<!-- <div class:active={active}> -->
+<div class:active>
+  Hello
+</div>
+
+<!-- 요소의 스타일 속성 바인딩 -->
+<!-- <h2 style="background-color: {color.t}; color: {color.w}; {letterSpacing}"> -->
+<h2
+  style:background-color={color.t}
+  style:color={color.w}
+  style={letterSpacing}
+>
+  Jinmok!
+</h2>
+
+<style>
+  div {
+    width: 120px;
+    height: 200px;
+    background-color: royalblue;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 20px;
+    transition: .4s;
+  }
+
+  .active {
+    width: 250px;
+    background-color: tomato;
+  }
+</style>
