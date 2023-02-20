80f1da969a7db2813010a50075a1996d033829f5
ChoJinmok <jjmok1127@gmail.com>
2023-02-21 00:59:41 +0900

    Add: 조건과 반복과 키 / 3. 반복 블록 패턴 정리

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
-  // let fruits = ['Apple', 'Banana', 'Cherry', 'Apple', 'Orange'];
-  // => 각각의 아이템으로 key를 주려고 하면 Apple이 중복되므로 고유하지가 않다.
-  // => 보통 id를 만들어준다.
-
-  let fruits = [
+  const fruits = [
     { id: 1, name: 'Apple' },
     { id: 2, name: 'Banana' },
     { id: 3, name: 'Cherry' },
@@ -11,21 +7,91 @@
     { id: 5, name: 'Orange' },
   ];
 
-  function deleteFirst() {
-    fruits = fruits.slice(1);
-  }
+  const todos = [];
+
+  const fruits2D = [
+    [1, 'Apple'],
+    [2, 'Banana'],
+    [3, 'Cherry'],
+    [4, 'Orange'],
+  ];
+
+  const user = {
+    name: 'Jinmok',
+    age: 85,
+    email: 'jjmok1127@gmail.com',
+  };
 </script>
 
-<button on:click={deleteFirst}>
-  Delete first fruit!
-</button>
-
-<!--
-  key를 통해서 반복되는 아이템에 고유성을 부여할 수 있다.
-  만약 아이템이 기존의 key와 동일하다면 다시 렌더링하지 않는다 -> 화면을 갱신해서 렌더링하는 비용이 감소
- -->
-<ul>
-  {#each fruits as { id, name } (id)}
-    <li>{name}</li>
+<section>
+  <h2>기본</h2>
+  <!-- {#each 배열 as 속성} {/each} -->
+  {#each fruits as fruit}
+    <div>{fruit.name}</div>
   {/each}
-</ul>
+</section>
+
+<section>
+  <h2>순서(index)</h2>
+  <!-- {#each 배열 as 속성, 순서} {/each} -->
+  {#each fruits as fruit, index}
+    <div>{index} / {fruit.name}</div>
+  {/each}
+</section>
+
+<section>
+  <h2>아이템 고유화(key)</h2>
+  <!-- {#each 배열 as 속성, 순서 (키)} {/each} -->
+  {#each fruits as fruit, index (fruit.id)}
+    <div>{index} / {fruit.name}</div>
+  {/each}
+</section>
+
+<section>
+  <!-- undefined, null은 적용 안된다. -->
+  <h2>빈 배열 처리(else)</h2>
+  <!-- {#each} {:else} {/each} -->
+  {#each todos as todo (todo.id)}
+    <div>{todo.name}</div>
+  {:else}
+    <div>아이템이 없어요!</div>
+  {/each}
+</section>
+
+<section>
+  <h2>구조 분해(destucturing)</h2>
+  <!-- {#each 배열 as { id, name } (id)} {/each} -->
+  {#each fruits as { id, name } (id)}
+    <div>{name}</div>
+  {/each}
+</section>
+
+<section>
+  <h2>2차원 배열</h2>
+  <!-- {#each 배열 as [id, name] (id)} {/each} -->
+  {#each fruits2D as [id, name] (id)}
+    <div>{name}</div>
+  {/each}
+</section>
+
+<section>
+  <h2>나머지 연산자(reset)</h2>
+  <!-- {#each 배열 as { id, ...rest } (id)} {/each} -->
+  {#each fruits as { id, ...rest } (id)}
+    <div>{rest.name}</div>
+  {/each}
+</section>
+
+<section>
+  <h2>객체 데이터</h2>
+  {#each Object.entries(user) as [key, value] (key)}
+    <div>{key}: {value}</div>
+  {/each}
+</section>
+
+<style>
+  section {
+    border: 1px solid orange;
+    margin-bottom: 10px;
+  }
+</style>
