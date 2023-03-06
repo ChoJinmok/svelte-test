f129725fe34752e3433e74421c409a128c24c842
ChoJinmok <jjmok1127@gmail.com>
2023-03-06 23:32:37 +0900

    Add: 비동기 / 3. 영화 검색 API 예제 만들기

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,49 @@
 <script lang="ts">
-  import Count from './Count.svelte';
+  import axios from 'axios';
 
-  let reset = false;
+  const apiKey = '377400ef';
+  let title = '';
+  let movies = null;
+  let error = null;
+  let loading = false;
 
-  function handleClick() {
-    reset = !reset;
+  async function searchMovies() {
+    // 이미 loading 중이면 함수 종료
+    if (loading) return;
+
+    movies = null;
+    error = null;
+    loading = true;
+
+    const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${title}`;
+    try {
+      const res = await axios.get(url);
+      movies = res.data.Search;
+    } catch (err) {
+      error = err;
+    } finally {
+      loading = false;
+    }
   }
 </script>
 
-<!--
-  Key 블록은 연결된 데이터의 값이 변경될 때마다 내용을 파괴하고 다시 생성
- -->
-{#key reset}
-  <Count />
-{/key}
+<input bind:value={title} />
+<button on:click={searchMovies}>검색!</button>
 
-<button on:click={handleClick}>
-  Reset!
-</button>
+{#if loading}
+  <p class="text-blue-400">Loading...</p>
+{:else if movies}
+  <ul>
+    {#each movies as movie}
+      <li>{movie.Title}</li>
+    {/each}
+  </ul>
+{:else if error}
+  <p class="text-red-400">{error.message}</p>
+{/if}
+
+<style lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
