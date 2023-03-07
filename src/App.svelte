db7c8daa39110312ab0800c8ceb78a22f21c4836
ChoJinmok <jjmok1127@gmail.com>
2023-03-08 00:22:49 +0900

    Add: 비동기 / 5. Await 블록으로 영화 검색 API 예제 리팩토링

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,51 @@
 <script lang="ts">
-  const isError = false;
-  // let promise = new Promise((resolve) => {
-  //   resolve('Hmm...');
-  // });
-  let promise = Promise.resolve('Hmm...');
+  import axios from 'axios';
 
-  function fetchName() {
-    return new Promise((resolve, reject) => {
-      if (isError) {
-        reject(new Error('Sorry...'));
-      }
+  const apiKey = '377400efd';
+  let title = '';
+  // let promise = new Promise((resolve) => { resolve([]); });
+  let promise = Promise.resolve([]);
 
-      setTimeout(() => {
-        resolve('Jinmok');
-      }, 2000);
-    });
+  async function searchMovies() {
+    const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${title}`;
+    // return new Promise(async (resolve, reject) => {
+    //   try {
+    //     const res = await axios.get(url);
+    //     resolve(res.data.Search);
+    //   } catch (err) {
+    //     reject(err);
+    //   }
+    // });
+    try {
+      const { data } = await axios.get(url);
+      return data.Search;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  function handleSearchClick() {
+    promise = searchMovies();
   }
 </script>
 
-<button
-  on:click={() => {
-    promise = fetchName();
-
-  // 아래와 같은 방법도 가능
-    // console.log(await promise);
-    // promise.then((res) => {
-    //   console.log(res);
-    // });
-  }}>
-  Fetch name!
-</button>
+<input bind:value={title} />
+<button on:click={handleSearchClick}>검색!</button>
 
 {#await promise}
-  <!-- 대기(Pending) -->
-  <!-- promise 객체를 기다리는 동안 보여줄 항목 작성  -->
-  <p>Loading...</p>
-
-  <!-- promise 객체가 기다려져서 반환되는 데이터를 담을 변수를 then에서 선언해준다. -->
-{:then name}
-  <!-- 이행(Fulfilled) -->
-  <h1>{name}</h1>
-
-  <!-- error가 발생했을 때 error 데이터를 담을 변수를 catch에서 선언해준다. -->
+  <p class="text-blue-400">Loading...</p>
+{:then movies}
+  <ul>
+    {#each movies as movie}
+      <li>{movie.Title}</li>
+    {/each}
+  </ul>
 {:catch error}
-  <!-- 거부(Rejected) -->
-  <p>{error.message}</p>
+  <p class="text-red-400">{error.message}</p>
 {/await}
 
-<!-- promise 객체를 await블록에서 실행하는데 대기,거부가 필요없고 이행만 필요한 경우 -->
-
-{#await promise then name}
-  <!-- 이행(Fulfilled) -->
-  <h1>{name}</h1>
-{/await}
+<style lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
