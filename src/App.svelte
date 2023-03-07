0d8faeb3bb49b181a17217b52c815333ffece249
ChoJinmok <jjmok1127@gmail.com>
2023-03-07 23:31:30 +0900

    Add: 비동기 / 4. Await 블록

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,55 @@
 <script lang="ts">
-  import axios from 'axios';
+  const isError = false;
+  // let promise = new Promise((resolve) => {
+  //   resolve('Hmm...');
+  // });
+  let promise = Promise.resolve('Hmm...');
 
-  const apiKey = '377400ef';
-  let title = '';
-  let movies = null;
-  let error = null;
-  let loading = false;
+  function fetchName() {
+    return new Promise((resolve, reject) => {
+      if (isError) {
+        reject(new Error('Sorry...'));
+      }
 
-  async function searchMovies() {
-    // 이미 loading 중이면 함수 종료
-    if (loading) return;
-
-    movies = null;
-    error = null;
-    loading = true;
-
-    const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${title}`;
-    try {
-      const res = await axios.get(url);
-      movies = res.data.Search;
-    } catch (err) {
-      error = err;
-    } finally {
-      loading = false;
-    }
+      setTimeout(() => {
+        resolve('Jinmok');
+      }, 2000);
+    });
   }
 </script>
 
-<input bind:value={title} />
-<button on:click={searchMovies}>검색!</button>
+<button
+  on:click={() => {
+    promise = fetchName();
 
-{#if loading}
-  <p class="text-blue-400">Loading...</p>
-{:else if movies}
-  <ul>
-    {#each movies as movie}
-      <li>{movie.Title}</li>
-    {/each}
-  </ul>
-{:else if error}
-  <p class="text-red-400">{error.message}</p>
-{/if}
+  // 아래와 같은 방법도 가능
+    // console.log(await promise);
+    // promise.then((res) => {
+    //   console.log(res);
+    // });
+  }}>
+  Fetch name!
+</button>
 
-<style lang="postcss">
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
-</style>
+{#await promise}
+  <!-- 대기(Pending) -->
+  <!-- promise 객체를 기다리는 동안 보여줄 항목 작성  -->
+  <p>Loading...</p>
+
+  <!-- promise 객체가 기다려져서 반환되는 데이터를 담을 변수를 then에서 선언해준다. -->
+{:then name}
+  <!-- 이행(Fulfilled) -->
+  <h1>{name}</h1>
+
+  <!-- error가 발생했을 때 error 데이터를 담을 변수를 catch에서 선언해준다. -->
+{:catch error}
+  <!-- 거부(Rejected) -->
+  <p>{error.message}</p>
+{/await}
+
+<!-- promise 객체를 await블록에서 실행하는데 대기,거부가 필요없고 이행만 필요한 경우 -->
+
+{#await promise then name}
+  <!-- 이행(Fulfilled) -->
+  <h1>{name}</h1>
+{/await}
