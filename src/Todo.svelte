aaefc3d952dce8c16f4b09c1f7eff7cd675e4f45
ChoJinmok <jjmok1127@gmail.com>
2023-03-15 01:38:34 +0900

    Add: 컴포넌트 / 3. Props 양방향 바인딩

---
+++ src/Todo.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { TodoType } from './types';
+
+  export let title: string;
+  export let done: boolean;
+  export let todos: TodoType[];
+  export let index: number;
+
+  // export let deleteTodo: (id: number) => void;
+
+    // function onClick() {
+    //   deleteTodo(id);
+    // }
+
+  function deleteTodo() {
+    todos.splice(index, 1);
+    todos = todos;
+  }
+</script>
+
+<div>
+  <input
+    type="checkbox"
+    bind:value={done} />
+  <span>{title}</span>
+  <button on:click={deleteTodo}>x</button>
+</div>
