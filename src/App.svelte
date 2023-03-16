bfb2635e88a5b293c466f26ea69ba16c3f1b31c6
ChoJinmok <jjmok1127@gmail.com>
2023-03-17 02:05:10 +0900

    Add: 컴포넌트 / 5. 기본/커스텀 이벤트 포워딩(Forwarding)

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-  import Todo from './Todo.svelte';
+  import Parent from './Parent.svelte';
 
-  let todos = [
-    { id: 1, title: 'Breakfast', done: false },
-    { id: 2, title: 'Lunch', done: false },
-    { id: 3, title: 'Dinner', done: false },
-  ];
+  // function handler({ detail }: CustomEvent) {
+  //   const event = detail.event as Event;
+  //   console.log(event.currentTarget);
+  // }
 
-  // `createEventDispatcher` 함수에서 `new CustomEvent()`를 통해 커스텀 이벤트를 생성한다.
-  // 따라서 `event.detail`을 사용할 수 있다.
-  function deleteTodo({ detail: todo }: CustomEvent) {
-    todos = [...todos].filter(({ id }) => id !== todo.id);
+  function handler({ currentTarget }: CustomEvent) {
+    console.log(currentTarget);
+  }
+
+  function myEventHandler({ detail }: CustomEvent) {
+    console.log(detail.myName);
   }
 </script>
 
-{#each todos as todo (todo.id)}
-  <Todo
-    {...todo}
-    on:deleteMe={deleteTodo} />
-{/each}
+<Parent
+  on:click={handler}
+  on:myEvent={myEventHandler}
+/>
---
+++ src/Child.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  function handleClick() {
+    dispatch('myEvent', {
+      myName: 'Jinmok!!',
+    });
+  }
+</script>
+
+<h2>Child</h2>
+<button on:click={handleClick}>
+  Child click!
+</button>
